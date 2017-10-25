36ebfc99571d3d6444000d2762f04b679d266bf2
Nbazylevich <NBazylevich@it-strategy.spb.ru>
2017-10-25 15:26:39 +0300

    bag fix - только для первого слайдера

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
         var link = this;
         setTimeout(function(){
             var curCarouuselIndx = $(link)[0].hash.substring($(link)[0].hash.indexOf("l")+1);
-            var curCarousel = $("#panel" + curCarouuselIndx);
+            var curCarousel = $(".telezki_vid_slaider").find("#panel" + curCarouuselIndx);
 
             var itemsCount = curCarousel.find('div.owl-stage').find('div.owl-item').length;
             var indxActiveDot = curCarousel.find('.owl-dot.active').index();
@@ -45,7 +45,7 @@ $(document).ready(function(){
             $(vis_owl_items[indxActiveDot]).addClass("active");
             var indxNewActive = $(vis_owl_items[indxActiveDot]).index();
 
-            $(".owl-stage").width($(".owl-item").width() * (itemsCount));
+            $(".telezki_vid_slaider").find(".owl-stage").width($(".owl-item").width() * (itemsCount));
             curCarousel.find(".owl-stage").css("transform", "translate3d("+(-indxNewActive*$(".owl-item").width())+"px, 0px, 0px)");
         }, 400);
         setOptionSlider()
@@ -54,23 +54,23 @@ $(document).ready(function(){
     $( window ).resize(function() {
         setTimeout(function(){
             var w2 = $(".telezki_vid_slaider").width();
-            $(".owl-item").width(w2);
-            var itemsCount = $(".tab-pane.active").find('div.owl-stage').find('div.owl-item').length;
-            var indxActive =$(".tab-pane.active").find('div.owl-stage').find('div.active').index();
-            $(".owl-stage").width($(".owl-item").width() * (itemsCount));
-            $(".tab-pane.active").find(".owl-stage").css("transform", "translate3d("+(-indxActive*$(".owl-item").width())+"px, 0px, 0px)");
+            $(".telezki_vid_slaider").find(".owl-item").width(w2);
+            var itemsCount = $(".telezki_vid_slaider").find(".tab-pane.active").find('div.owl-stage').find('div.owl-item').length;
+            var indxActive = $(".telezki_vid_slaider").find(".tab-pane.active").find('div.owl-stage').find('div.active').index();
+            $(".telezki_vid_slaider").find(".owl-stage").width($(".owl-item").width() * (itemsCount));
+            $(".telezki_vid_slaider").find(".tab-pane.active").find(".owl-stage").css("transform", "translate3d("+(-indxActive*$(".owl-item").width())+"px, 0px, 0px)");
         }, 500)
     });
 
 
 
     function setOptionSlider(){
-        $(".owl-dot").click(function(event){
+        $(".telezki_vid_slaider").find(".owl-dot").click(function(event){
             event.preventDefault();
             event.stopPropagation();
-            $(".owl-stage").addClass("owl-stage-animation");
+            $(".telezki_vid_slaider").find(".owl-stage").addClass("owl-stage-animation");
             var indexItem = $(this).index();
-            var owl_items = $(".tab-pane.active").find('div.owl-stage').find('div.owl-item');
+            var owl_items = $(".telezki_vid_slaider").find(".tab-pane.active").find('div.owl-stage').find('div.owl-item');
             var vis_owl_items = [];
             for(var oi = 0; oi < owl_items.length; oi++){
                 if(!$(owl_items[oi]).hasClass('cloned')){
@@ -81,7 +81,7 @@ $(document).ready(function(){
             $(vis_owl_items[indexItem]).addClass("active");
             $(this).parent().find(".owl-dot").removeClass("active");
             $(this).addClass("active");
-            $(".tab-pane.active").find(".owl-stage").css("transform", "translate3d("+(-($(this).index() + 2)*$(".owl-item").width())+"px, 0px, 0px)");
+            $(".telezki_vid_slaider").find(".tab-pane.active").find(".owl-stage").css("transform", "translate3d("+(-($(this).index() + 2)*$(".owl-item").width())+"px, 0px, 0px)");
         })
     }
 
